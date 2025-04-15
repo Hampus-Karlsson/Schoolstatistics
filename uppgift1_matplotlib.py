83374b7609f561dfed107b1cbfe44cdd5930a5ec
Hampus Karlsson <hampus04.karlsson@gmail.com>
2025-04-15 11:43:02 +0200

    Added code to read sheets

--- uppgift1_matplotlib.py
+++ uppgift1_matplotlib.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+
+# Create file if not already exist
+os.makedirs("visualiseringar", exist_ok=True)
+
+# Read from arcs
+file = "data/riket2023_åk9_np.xlsx"
+arc_name = ["english", "math", "swedish", "swedish as secondlanguage"]
+
+# Store all dataframes in list
+dataframes = [pd.read_excel(file, sheet_name=sheet) for sheet in arc_name]
+
+# Subjects (Headings)
+subjects = ["English", "Math", "SWedish", "Swedish as secondlanguage"]
